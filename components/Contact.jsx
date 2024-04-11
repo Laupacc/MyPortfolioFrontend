c7bcb137df966b4acf5ffba4668882fa6a4a055d
Your name <Your email>
2024-04-11 12:35:02 +0200

    more setup

---
+++ components/Contact.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Contact.module.css";
+
+function Contact() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Contact Page!</h1>
+      </main>
+    </div>
+  );
+}
+export default Contact;
---
+++ components/Projects.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Projects.module.css";
+
+function Projects() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Projects Page!</h1>
+      </main>
+    </div>
+  );
+}
+export default Projects;
