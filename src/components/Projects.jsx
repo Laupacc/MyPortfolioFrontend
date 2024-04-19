9237cd8ce078ee0f8856d8003d1804c63008d9ae
Your name <Your email>
2024-04-19 17:19:19 +0200

    dd

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,10 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import projectsData from "../data/projectsData.json";
+import { DeviceFrameset } from "react-device-frameset";
+import "react-device-frameset/styles/marvel-devices.min.css";
+
+import { IPhoneX } from "react-device-frames";
 
 function Projects() {
   return (
@@ -13,92 +17,67 @@ function Projects() {
       <p className={styles.scrollText}>Scroll ⇨ for more projects</p>
       <main className={styles.main}>
         <Fade right>
-          <div className={styles.cardContainer}>
-            {projectsData.map((project) => (
-              <Card
-                key={project.id}
-                sx={(theme) => ({
-                  borderRadius: theme.spacing(2),
-                  transition: "0.3s",
-                  boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
-                  position: "relative",
-                  width: 400,
-                  height: 300,
-                  marginLeft: theme.spacing(6),
-                  marginRight: theme.spacing(6),
-                  marginBottom: theme.spacing(4),
-                  marginTop: theme.spacing(4),
-                  overflow: "initial",
-                  background: "#ffffff",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  paddingBottom: theme.spacing(2),
-                  [theme.breakpoints.up("md")]: {
-                    flexDirection: "row",
-                    paddingTop: theme.spacing(2),
-                  },
-                })}
-              >
-                <CardMedia
-                  image={project.imageUrl}
-                  sx={(theme) => ({
-                    width: "80%",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    marginTop: theme.spacing(-3),
-                    height: 0,
-                    paddingBottom: "48%",
-                    borderRadius: theme.spacing(2),
-                    position: "relative",
-                    [theme.breakpoints.up("md")]: {
-                      width: "100%",
-                      marginLeft: theme.spacing(-3),
-                      marginTop: 0,
-                      transform: "translateX(-8px)",
-                    },
-                  })}
-                />
-                <CardContent>
-                  <Box>
-                    <Box
-                      component="h2"
-                      sx={{
-                        fontWeight: "bold",
-                        fontSize: 20,
-                        marginBottom: "0.35em",
-                      }}
-                    >
-                      {project.title}
-                    </Box>
-                    <Box
-                      sx={{
-                        fontSize: "0.8rem",
-                        marginBottom: "0.35em",
-                      }}
-                    >
-                      {project.subtitle}
-                    </Box>
-                    <Button
-                      href={project.demoLink}
-                      target="_blank"
-                      sx={{
-                        backgroundImage:
-                          "linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)",
-                        boxShadow: "0px 4px 32px rgba(252, 56, 56, 0.4)",
-                        borderRadius: 100,
-                        paddingLeft: 3,
-                        paddingRight: 3,
-                        color: "#ffffff",
-                      }}
-                    >
-                      Demo
-                    </Button>
-                  </Box>
-                </CardContent>
-              </Card>
+          <div className={styles.webContainer}>
+            {projectsData.webProjects.map((project) => (
+              <div className={styles.card}>
+                <DeviceFrameset device="MacBook Pro" zoom={"55%"}>
+                  <iframe src={project.demoLink} width="100%" height="100%" />
+                  <img className={styles.imageUrl} src={project.imageUrl} />
+                </DeviceFrameset>
+                <div className={styles.content}>
+                  <div className={styles.title}>{project.title}</div>
+                  <div className={styles.subtitle}>{project.subtitle}</div>
+                  <Button
+                    className={styles.demoLink}
+                    href={project.demoLink}
+                    target="_blank"
+                  >
+                    To website
+                  </Button>
+                </div>
+              </div>
             ))}
           </div>
+          <div className={styles.phoneContainer}>
+            {projectsData.phoneProjects.map((project) => (
+              <div className={styles.card}>
+                <DeviceFrameset device="iPhone X" zoom={"60%"}>
+                  <img src={project.imageUrl} width="100%" height="100%" />
+                </DeviceFrameset>
+                <div className={styles.content}>
+                  <div className={styles.title}>{project.title}</div>
+                  <div className={styles.subtitle}>{project.subtitle}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* <div class={styles.containerphone}>
+            <div class={styles.phone}>
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1543663/cp-iphonex-1125x2436.png" />
+            </div>
+          </div> */}
+
+          {/* <IPhoneX
+            screenshot={"https://i.imgur.com/oLOEoYv.png"}
+            height={500}
+          /> */}
+
+          {/* {projectsData.map((project) => (
+            <div className={styles.computer}>
+              <div className={styles.stand}>
+                <div
+                  className={styles.monitor}
+                  style={{ backgroundImage: `url(${project.imageUrl})` }}
+                ></div>
+              </div>
+              <div>{project.title}</div>
+              <div>{project.subtitle}</div>
+              <Button href={project.demoLink} target="_blank">
+                Demo
+              </Button>
+            </div>
+          ))} */}
         </Fade>
       </main>
     </>
