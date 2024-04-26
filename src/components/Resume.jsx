76ab8fd1e71066514a5625d2d5703dff2689e55e
Your name <Your email>
2024-04-26 15:19:46 +0200

    v

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
+import IconButton from "@mui/material/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 
 function Resume() {
@@ -28,8 +29,8 @@ function Resume() {
   const useStyles = makeStyles((theme) => ({
     iconColor: {
       color: "#f05334",
-      width: "45px",
-      height: "45px",
+      width: "3rem",
+      height: "3rem",
     },
   }));
   const classes = useStyles();
