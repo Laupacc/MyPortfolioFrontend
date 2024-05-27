38ea763b9194a20d041a004f004070fe17cedb7a
Your name <Your email>
2024-05-27 20:53:13 +0200

    mediaquery

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,53 +9,16 @@ import {
   FaJsSquare,
   FaGitAlt,
 } from "react-icons/fa";
+import { useMediaQuery } from "react-responsive";
 
 function About() {
   const { t } = useTranslation();
 
+  const isDesktop = useMediaQuery({ minWidth: 768 });
   return (
     <>
-      <div className={styles.fadeMobile}>
-        <Fade bottom>
-          <div className={styles.main}>
-            <div className={styles.container}>
-              <div className={styles.wavy}>
-                <span style={{ "--i": 1 }}>{t("about.title.1")}</span>
-                <span style={{ "--i": 2 }}>{t("about.title.2")}</span>
-                <span style={{ "--i": 3 }}>{t("about.title.3")}</span>
-                <span style={{ "--i": 4 }}>{t("about.title.4")}</span>
-                <span style={{ "--i": 5 }}>{t("about.title.5")}</span>
-              </div>
-              <div className={styles.aboutText}>{t("about.description")}</div>
-
-              <div className={styles.stageCubeCont}>
-                <div className={styles.cubeSpinner}>
-                  <div className={styles.face1}>
-                    <FaNode color="#4B8BBE" size={160} />
-                  </div>
-                  <div className={styles.face2}>
-                    <FaHtml5 color="#F06529" size={160} />
-                  </div>
-                  <div className={styles.face3}>
-                    <FaCss3 color="#28A4D9" size={160} />
-                  </div>
-                  <div className={styles.face4}>
-                    <FaReact color="#5ED4F4" size={160} />
-                  </div>
-                  <div className={styles.face5}>
-                    <FaJsSquare color="#EFD81D" size={160} />
-                  </div>
-                  <div className={styles.face6}>
-                    <FaGitAlt color="#EC4D28" size={160} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Fade>
-      </div>
-      <div className={styles.fadeDesktop}>
-        <Fade right>
+      <div className={isDesktop ? styles.fadeDesktop : styles.fadeMobile}>
+        <Fade {...(isDesktop ? { right: true } : { bottom: true })}>
           <div className={styles.main}>
             <div className={styles.container}>
               <div className={styles.wavy}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Fade from "react-reveal/Fade";
 import { DNA, RotatingTriangles } from "react-loader-spinner";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useMediaQuery } from "react-responsive";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -15,6 +16,7 @@ function Home() {
   }, []);
 
   const { t } = useTranslation();
+  const isDesktop = useMediaQuery({ minWidth: 768 });
 
   return (
     <>
@@ -29,23 +31,8 @@ function Home() {
         />
       ) : (
         <div className={styles.main}>
-          <div className={styles.fadeMobile}>
-            <Fade bottom>
-              <div className={styles.container}>
-                <div className={styles.wavy}>
-                  <span style={{ "--i": 1 }}>{t("home.title.1")}</span>
-                  <span style={{ "--i": 2 }}>{t("home.title.2")}</span>
-                  <span style={{ "--i": 3 }}>{t("home.title.3")}</span>
-                  <span style={{ "--i": 4 }}>{t("home.title.4")}</span>
-                  <span style={{ "--i": 5 }}>{t("home.title.5")}</span>
-                </div>
-                <div className={styles.maintext}>{t("home.description")}</div>
-              </div>
-            </Fade>
-          </div>
-
-          <div className={styles.fadeDesktop}>
-            <Fade right>
+          <div className={isDesktop ? styles.fadeDesktop : styles.fadeMobile}>
+            <Fade {...(isDesktop ? { right: true } : { bottom: true })}>
               <div className={styles.container}>
                 <div className={styles.wavy}>
                   <span style={{ "--i": 1 }}>{t("home.title.1")}</span>
