ecc90f41a390d0d05a3353392cceb3a9ee1578a7
Your name <Your email>
2024-06-10 14:30:57 +0200

    cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,53 +1,40 @@
 import styles from "../styles/Home.module.css";
 import Fade from "react-reveal/Fade";
-import { DNA, RotatingTriangles } from "react-loader-spinner";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useMediaQuery } from "react-responsive";
+import { useEffect, useState } from "react";
 
 function Home() {
-  const [loading, setLoading] = useState(true);
+  const { t } = useTranslation();
+  const [isDesktop, setIsDesktop] = useState(false);
+  const isDesktopQuery = useMediaQuery({ minWidth: 768 });
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-    return () => clearTimeout(timer);
-  }, []);
+    const timeoutId = setTimeout(() => {
+      setIsDesktop(isDesktopQuery);
+    }, 150); // delay in milliseconds
 
-  const { t } = useTranslation();
-  const isDesktop = useMediaQuery({ minWidth: 768 });
+    // Cleanup function to clear the timeout if the component unmounts
+    return () => clearTimeout(timeoutId);
+  }, [isDesktopQuery]);
 
   return (
-    <>
-      {/* {loading ? (
-        <DNA
-          visible={true}
-          height="140"
-          width="140"
-          ariaLabel="dna-loading"
-          wrapperClass={styles.dnaWrapper}
-          wrapperStyle={{}}
-        />
-      ) : ( */}
-      <div className={styles.main}>
-        <div className={isDesktop ? styles.fadeDesktop : styles.fadeMobile}>
-          <Fade {...(isDesktop ? { right: true } : { bottom: true })}>
-            <div className={styles.container}>
-              <div className={styles.wavy}>
-                <span style={{ "--i": 1 }}>{t("home.title.1")}</span>
-                <span style={{ "--i": 2 }}>{t("home.title.2")}</span>
-                <span style={{ "--i": 3 }}>{t("home.title.3")}</span>
-                <span style={{ "--i": 4 }}>{t("home.title.4")}</span>
-                <span style={{ "--i": 5 }}>{t("home.title.5")}</span>
-              </div>
-              <div className={styles.maintext}>{t("home.description")}</div>
+    <div className={styles.main}>
+      <div className={isDesktop ? styles.fadeDesktop : styles.fadeMobile}>
+        <Fade {...(isDesktop ? { right: true } : { bottom: true })}>
+          <div className={styles.container}>
+            <div className={styles.wavy}>
+              <span style={{ "--i": 1 }}>{t("home.title.1")}</span>
+              <span style={{ "--i": 2 }}>{t("home.title.2")}</span>
+              <span style={{ "--i": 3 }}>{t("home.title.3")}</span>
+              <span style={{ "--i": 4 }}>{t("home.title.4")}</span>
+              <span style={{ "--i": 5 }}>{t("home.title.5")}</span>
             </div>
-          </Fade>
-        </div>
+            <div className={styles.maintext}>{t("home.description")}</div>
+          </div>
+        </Fade>
       </div>
-      {/* )} */}
-    </>
+    </div>
   );
 }
 
